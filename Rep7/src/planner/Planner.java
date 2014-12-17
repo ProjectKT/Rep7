aad3789f96e2e29b7b33a4de86422eaf3e3fce5f
geukfyger <geukfyger.anti0pass@gmail.com>
2014-12-17 16:34:53 +0900

    GUIの試作

--- Rep7/src/planner/Planner.java
+++ Rep7/src/planner/Planner.java
@@ -182,8 +182,8 @@ public class Planner {
 
 	private ArrayList<Object> initGoalList() {
 		ArrayList<Object> goalList = new ArrayList<Object>();
-		goalList.add("B on C");
 		goalList.add("A on B");
+		goalList.add("B on C");
 		return goalList;
 	}
 
