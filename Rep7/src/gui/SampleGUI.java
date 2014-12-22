321105b17fe6a20f4c24640064bd122db69c6bd2
cjh15088 <cjh15088@cse>
2014-12-22 13:53:28 +0900

    gui調整

--- Rep7/src/gui/SampleGUI.java
+++ Rep7/src/gui/SampleGUI.java
@@ -30,6 +30,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.FlowLayout;
 import javax.swing.JTextArea;
+import javax.swing.JLabel;
 
 public class SampleGUI extends JFrame{
 
@@ -83,11 +84,18 @@ public class SampleGUI extends JFrame{
 		panel.add(tabbedPane, BorderLayout.WEST);
 		
 		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_1, null);
-		panel_1.setLayout(null);
+		tabbedPane.addTab("初期状態", null, panel_1, null);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea = new JTextArea();
+		panel_1.add(textArea, BorderLayout.CENTER);
 		
 		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_2, null);
+		tabbedPane.addTab("目標状態", null, panel_2, null);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea_2 = new JTextArea();
+		panel_2.add(textArea_2, BorderLayout.CENTER);
 		
 		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane_2.setPreferredSize(new Dimension(200, 100));
@@ -95,6 +103,10 @@ public class SampleGUI extends JFrame{
 		
 		JPanel panel_5 = new JPanel();
 		tabbedPane_2.addTab("New tab", null, panel_5, null);
+		panel_5.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea_1 = new JTextArea();
+		panel_5.add(textArea_1, BorderLayout.CENTER);
 		
 		JTabbedPane tabbedPane_3 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane_3.setPreferredSize(new Dimension(150, 200));
@@ -129,7 +141,13 @@ public class SampleGUI extends JFrame{
 		
 		panel_4.setBorder(border4);
 		panel_8.setBorder(border8);
+		
+		JLabel label = new JLabel("初期状態");
+		panel_8.add(label);
 		panel_9.setBorder(border9);
+		
+		JLabel label_1 = new JLabel("目標状態");
+		panel_9.add(label_1);
 	}
 	
 	private void loadData(){
